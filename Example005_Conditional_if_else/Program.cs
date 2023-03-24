4fd1eedc025d3d3a58ce4f2c7ae6d326aee352a2
VladimirRudakov <113288121+VladimirRudakov@users.noreply.github.com>
2023-03-24 09:09:32 +0300

    Condition

---
+++ Example005_Conditional_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя:cd ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.Write("Приветб ");
+    Console.WriteLine(username);
+}
