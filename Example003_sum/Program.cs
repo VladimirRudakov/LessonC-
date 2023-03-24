a477eaccded34e512ac3eca70ab00422014d28aa
VladimirRudakov <113288121+VladimirRudakov@users.noreply.github.com>
2023-03-24 08:58:05 +0300

    Random

--- Example003_sum/Program.cs
+++ Example003_sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 3;
-int NumberB = 5;
+﻿int numberA = new Random().Next(1,10);
+Console.WriteLine(numberA);
+int NumberB = new Random().Next(1,10);
+Console.WriteLine(NumberB);
 int result = numberA + NumberB;
 Console.WriteLine(result);
